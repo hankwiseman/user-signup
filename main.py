0fa7f1b52515ae1659e414dbf3a05b76144b1f13
hankwiseman <hankwiseman@gmail.com>
2017-10-03 19:25:53 -0500

    built base of program

--- main.py
+++ main.py
@@ -5,59 +5,39 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-# a list of movies that nobody should have to watch
-terrible_movies = [
-    "Gigli",
-    "Star Wars Episode 1: Attack of the Clones",
-    "Paul Blart: Mall Cop 2",
-    "Nine Lives",
-    "Starship Troopers"
-]
-
-def get_current_watchlist():
-    # returns user's current watchlist--hard coded for now
-    return [ "Star Wars", "Minions", "Freaky Friday", "My Favorite Martian" ]
-
-
-@app.route("/crossoff", methods=['POST'])
-def crossoff_movie():
-    crossed_off_movie = request.form['crossed-off-movie']
-
-    if crossed_off_movie not in get_current_watchlist():
-        # the user tried to cross off a movie that isn't in their list,
-        # so we redirect back to the front page and tell them what went wrong
-        error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
-
-        # redirect to homepage, and include error as a query parameter in the URL
-        return redirect("/?error=" + error)
-
-    # if we didn't redirect by now, then all is well
-    return render_template('crossoff.html', crossed_off_movie=crossed_off_movie)
-
-@app.route("/add", methods=['POST'])
-def add_movie():
+@app.route("/welcome", methods=['POST'])
+def welcome_screen():
     # look inside the request to figure out what the user typed
-    new_movie = request.form['new-movie']
+    user_name = request.form['username']
+    password = request.form['password']
+    verified_password = request.form['verified_password']
+    email = request.form['email']
 
     # if the user typed nothing at all, redirect and tell them the error
-    if (not new_movie) or (new_movie.strip() == ""):
-        error = "Please specify the movie you want to add."
+    if len(user_name) < 3 or len(user_name) > 20:
+        error = "The username is not between 3 and 20 characters."
         return redirect("/?error=" + error)
 
-    # if the user wants to add a terrible movie, redirect and tell them the error
-    if new_movie in terrible_movies:
-        error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
+    if len(password) < 3 or len(password) > 20:
+        error = "The password is not between 3 and 20 characters."
         return redirect("/?error=" + error)
+    
+    if password != verified_password:
+        error = "The passwords do not match"
+        return redirect("/?error=" + error)
+
+    
+
 
     # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-    new_movie_escaped = cgi.escape(new_movie, quote=True)
+    new_username_escaped = cgi.escape(user_name, quote=True)
 
-    return render_template('add-confirmation.html', movie=new_movie)
+    return render_template('welcome.html', username=user_name)
 
 
 @app.route("/")
 def index():
     encoded_error = request.args.get("error")
-    return render_template('edit.html', watchlist=get_current_watchlist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
+    return render_template('index.html', error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 app.run()
